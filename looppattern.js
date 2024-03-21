f27b1313c669ae3c991d91d052f2fa9041d9858b
hp03011999 <pooniahp98@gmail.com>
2024-03-21 13:51:42 +0530

    first commit

---
+++ looppattern.js
@@ -0,0 +1,154 @@
+for(let i=1; i <=5; i++){
+
+    for(let j = 1; j <= i; j++){
+        document.write("*");
+    }
+    document.write("<br>")
+}
+
+document.write("<br>")
+
+
+
+
+
+for(let i=1; i <=5; i++){
+
+    for(let j = 1; j <= i; j++){
+        document.write(j);
+    }
+    document.write("<br>")
+}
+
+document.write("<br>")
+
+
+
+
+
+document.write("<br>")
+
+
+
+
+
+
+
+
+for(let i = 5; i >= 1; i--){
+
+    for(let j= 5; j >= i; j--){
+
+        document.write(j);
+    }
+
+    document.write("<br>");
+}
+
+document.write("<br>")
+
+
+
+
+
+
+for(let i = 5; i>=1; i--){
+
+    for(let j= 1; j<=i; j++){
+
+        document.write(j);
+
+    }
+
+    document.write("<br>");
+   
+}
+
+document.write("<br>")
+
+
+
+
+
+
+
+
+
+
+for(let i= 1; i<=5; i++){
+
+    
+
+    for(let j = 1; j <= i; j++){
+
+
+        if(j % 2 === 0){
+
+            document.write("#");
+        }
+        else{
+            document.write("@");
+        }
+
+       
+        
+    }
+    document.write("<br>");
+}
+
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+for(let i = 5; i >= 1; i--){
+
+    for(let j = i; j >= 1; j--){
+
+        document.write(j);
+    }
+
+    document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+
+
+let k = 1;
+
+for(let i = 1; i <= 5; i++){
+
+    for( let j = 1 ; j <= i; j++ ){
+
+        document.write(k);
+        k++;
+    }
+    document.write("<br>");
+    
+}
+
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+
+
+
+let l = 15;
+
+for(let i = 5; i>=1; i--){
+
+    for(let j = 1; j <=i; j++){
+
+        document.write(l);
+        l--;
+    }
+    document.write("<br>");
+}
---
+++ whilrloop.js
@@ -0,0 +1,183 @@
+// // let a = 1;
+
+// // while (a <= 100) {
+
+
+// //     if (a >= 50 && a <= 60) {
+
+// //         continue;
+
+// //     } else if (a >= 80 && a <= 90) {
+
+// //         continue;
+// //     }
+
+// //     else {
+
+// //         document.write(a + "<br>");
+
+// //     }
+// //     a++;
+
+// // }
+
+
+// // while (a <= 100) {
+// //     if (a < 50) document.write(a + "<br>")
+// //     else if (a > 60 && a < 80) document.write(a + "<br>")
+// //     else if (a > 90) document.write(a + "<br>")
+// //     a++;
+// // }
+
+
+
+
+
+
+// // while (a <= 100) {
+// //     if (a >=1 && a <=49) document.write(a+ "<br>")
+// //     //  if (a > 60 && a < 80) document.write(a + "<br>")
+// //     // else if (a > 90) document.write(a + "<br>")
+// //      else if(a>=50&&a<=60)document.write("ram<br>");
+// //     else if( a >= 61 && a <=  100) document.write(a + "<br>")
+
+
+// //     a++;
+// // }
+
+
+
+
+
+
+
+// let i=1;
+
+// while(i<=5){
+//     let j=1;
+//     while(j<=i){
+//         document.write(j);
+//         j++;
+//     }
+//     document.write("<br>");
+//     i++;
+// }
+
+// document.write("<br>");
+// document.write("<br>");
+
+
+
+
+
+
+
+
+
+// let k = 1;
+
+// while(k <=5 ){
+
+//     let l = 1;
+
+//     while(l <= k){
+
+//         document.write("*");
+
+//         l++;
+
+
+//     }
+
+//     k++;
+//     document.write("<br>");
+// }
+
+
+
+
+
+// let m = 5;
+
+// while(m >= 1){
+
+//     let n = m;
+
+//     while(n >= 1){
+
+//         document.write(n);
+
+//         n--;
+//     }
+
+//     document.write("<br>");
+//     m--;
+// }
+
+// document.write("<br>");
+
+
+
+
+
+
+// let o = 1;
+// while(o <= 5){
+
+//     let p = 5;
+//     while(p >= o){
+
+//         document.write(p);
+//         p--;
+//     }
+
+//     document.write("<br>");
+//     o++;
+
+// }
+
+
+
+
+
+
+// let q = 5;
+// while(q >= 1){
+
+//     let r = 1;
+//     while(r<=q){
+
+//         document.write(r);
+//         r++;
+//     }
+
+//     document.write("<br>");
+//     q--;
+// }
+
+
+
+
+
+
+
+
+// k = 1;
+
+// for(i = 1; i <= 5; i++){
+//     for(j = 1; j <= i; j++){
+
+//         document.write(k);
+//         k= k+2;
+//     }
+//     document.write("<br>")
+// }
+
+
+
+
+
+
+
+
+
